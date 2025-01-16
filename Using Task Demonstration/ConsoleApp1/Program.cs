96a70f71b6acaacee2b170b94f6b58282ec574c4
LeDucDung168 <dungldhe173255@fpt.edu.vn>
2025-01-17 00:14:03 +0700

    done demo 1

---
+++ Using Task Demonstration/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void PrintNumber(string message)
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{message}: {i}");
+            Thread.Sleep(1000);
+        }
+    }
+    static void Main()
+    {
+        Thread.CurrentThread.Name = "Main";
+        //Create a task and pass a delegate to the method
+        Task task1 = new Task(() => PrintNumber("Task 1"));
+        task1.Start();
+        // create a task and pass a delegate to the method
+        Task task2 = Task.Run(delegate { PrintNumber("Task 2"); });
+        // Create a task by using a Action delegate
+        Task task3 = new Task(new Action(() => { PrintNumber("Task 3"); }));
+        task3.Start();
+        Console.WriteLine($"Thread '{Thread.CurrentThread.Name}'");
+        Console.ReadKey();
+    }
+}
