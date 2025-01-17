115db11176d94e0ac775406eb735f21010efa04f
LeDucDung168 <dungldhe173255@fpt.edu.vn>
2025-01-17 07:57:28 +0700

    fixxing demo4

--- Using Task Demonstration/Demo 4/Program.cs
+++ Using Task Demonstration/Demo 4/Program.cs
@@ -1,28 +1,35 @@
-﻿class Program
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
 {
     static void Main(string[] args)
     {
         var tasks = new List<Task<int>>();
         var source = new CancellationTokenSource();
         var token = source.Token;
-        int completedIterations = 0;
+        int completedInterations = 0;
 
         for (int n = 1; n <= 20; n++)
         {
             tasks.Add(Task.Run(() =>
             {
-                int iterations = 0;
+                int itegrations = 0;
                 for (int ctr = 1; ctr <= 2_000_000; ctr++)
                 {
                     token.ThrowIfCancellationRequested();
-                    iterations++;
+                    itegrations++;
                 }
-                Interlocked.Increment(ref completedIterations);
-                if (completedIterations >= 10) source.Cancel();
-                return iterations;
+                Interlocked.Increment(ref completedInterations);
+                if (completedInterations >= 10)
+                {
+                    source.Cancel();
+                }
+                return itegrations;
             }, token));
         }
-        Console.WriteLine("Waiting for the tasks to complete. \n");
+        Console.WriteLine("Waiting for the first 10 tasks to complete .... \n");
         try
         {
             Task.WaitAll(tasks.ToArray());
@@ -30,12 +37,15 @@
         catch (AggregateException)
         {
             Console.WriteLine("Status of tasks: \n");
-            Console.WriteLine("{0,10} {1,20} {2,14:N0}", "Task Id", "Status", "Interations");
-            foreach (var task in tasks)
+            Console.WriteLine("{0,10} {1,20} {2,14:N0}", "Task Id", "Status", "Iterations");
+            foreach (var t in tasks)
             {
-                Console.WriteLine("{0,10} {1,20} {2,14}", task.Id, task.Status, task.Status != TaskStatus.Canceled ? task.Result.ToString("N0") : "n/a");
+                Console.WriteLine("{0,10} {1,20} {2,14}",
+                    t.Id, t.Status,
+                    t.Status != TaskStatus.Canceled ? t.Result.ToString("N0") : "n/a");
             }
             Console.ReadLine();
         }
     }
+
 }
